218517318024478d1b1be2cc23da6b46ae479559
Sherartsu <jose.merino9108@gmail.com>
2025-05-28 21:35:57 -0600

    mejoras en capacitaciones

--- js/trainings.js
+++ js/trainings.js
@@ -3,17 +3,22 @@
 let btnOpenAddTraining = document.getElementById('btnOpenAddTraining');
 let btnCloseAddTraining = document.getElementById('btnCloseAddTraining');
 let formAddTraining = document.getElementById('formAddTraining');
+let modal = document.querySelector('.containerModalAddTraining');
 
 btnOpenAddTraining.addEventListener('click', () => {
-    let modal = document.querySelector('.containerModalAddTraining');
     modal.classList.replace('containerModalAddTrainingClose', 'containerModalAddTrainingOpen');
 })
 
 btnCloseAddTraining.addEventListener('click', () => {
-    let modal = document.querySelector('.containerModalAddTraining');
     modal.classList.replace('containerModalAddTrainingOpen', 'containerModalAddTrainingClose');
 })
 
 formAddTraining.addEventListener('submit', (e) => {
     e.preventDefault();
+})
+
+document.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.classList.replace('containerModalAddTrainingOpen', 'containerModalAddTrainingClose');
+    }
 })
