7018d4ed7e9f0c8fc099bc22633405427b976296
Alfaro294 <jenniferalfaro0819@gmail.com>
2025-05-31 17:32:08 -0600

    Modal de empleados agregado, modificación del metodo de busqueda

--- js/employee.js
+++ js/employee.js
@@ -1,3 +1,13 @@
+'use strict'
+// Cargar tabla al inicio
+document.addEventListener('DOMContentLoaded', refreshTable);
+//Busqueda del empleado
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('searchEmployee').addEventListener('click', searchEmployee);
+});
+
+/*Agregar el evento onclick*/
+
 const employees = [ /*Variable que registra los empleados*/
   {
     dui: '12345678-9',
@@ -62,10 +72,56 @@ function searchEmployee() {
     });
 }
 /*Función para agregar un nuevo empleado*/
-function addEmployee() {
-    
-}
 
-// Cargar tabla al inicio
-document.addEventListener('DOMContentLoaded', refreshTable);
-/*Agregar el evento onclick*/
+let addEmployee = document.getElementById('addEmployee');
+let closeAddEmployee = document.getElementById('closeAddEmployee');
+let formAddEmployee = document.getElementById('formAddEmployee');
+let modal = document.querySelector('.containerModalAddEmployee');
+
+addEmployee.addEventListener('click', () => {
+  modal.classList.replace('containerModalAddEmployeeClose' , 'containerModalAddEmployeeOpen')
+})
+
+closeAddEmployee.addEventListener('click', () =>{
+  modal.classList.replace('containerModalAddEmployeeOpen', 'containerModalAddEmployeeClose');
+})
+
+formAddEmployee.addEventListener('submit', (e) => {
+  e.preventDefault();
+})
+document.addEventListener('click', (e) => {
+  if (e.target === modal){
+    modal.classList.replace('containerModalAddEmployeeOpen' , 'containerModalAddEmployeeClose')
+  }
+})
+/*Función para agregar foto, esto abrira el explorador de archivos*/
+const inputFoto = document.getElementById("fotoEmpleado");
+const btnAgregarFoto = document.getElementById("btnAgregarFoto");
+
+btnAgregarFoto.addEventListener("click", () => {
+  inputFoto.click();
+});
+
+inputFoto.addEventListener("change", () => {
+  const file = inputFoto.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      previewFoto.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+});
+/*Función para al momento de ingresar el nombre y apellido se detalle el nombre completo */
+const nombreInput = document.getElementById("txtAddEmployeeName");
+const apellidoInput = document.getElementById("txtAddEmployeeLastName");
+const fullNameHeader = document.getElementById("FullName");
+
+// Función para actualizar el título superior
+function actualizarNombreCompleto() {
+    const nombre = nombreInput.value.trim();
+    const apellido = apellidoInput.value.trim();
+    fullNameHeader.textContent = (nombre || apellido) ? `${nombre} ${apellido}` : "Empleado";
+}
+nombreInput.addEventListener("input", actualizarNombreCompleto);
+apellidoInput.addEventListener("input", actualizarNombreCompleto);
