9d9056ca63693f5fb80f38b1a7b1d01b471c3dab
Alfaro294 <jenniferalfaro0819@gmail.com>
2025-06-08 18:47:41 -0600

    Estructura para modelo 3D

--- js/reportA.js
+++ js/reportA.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const modal = document.getElementById("modalEvidencias");
   const btnAbrir = document.getElementById("btnAgregar");
-  const cerrar = document.querySelector(".closeAddAccidente"); 
+  const cerrar = document.querySelector(".closeAddAccidente");
   const dropZone = document.getElementById("dropZone");
   const fileInput = document.getElementById("fileInput");
   const examinarBtn = document.getElementById("examinarBtn");
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
 fileInput.addEventListener("change", (e) => {
   const files = e.target.files;
   const previewContainer = document.getElementById("previewContainer");
-  previewContainer.innerHTML = ""; 
+  previewContainer.innerHTML = "";
   for (const file of files) {
     const reader = new FileReader();
     reader.onload = (event) => {
@@ -65,3 +65,59 @@ fileInput.addEventListener("change", (e) => {
     reader.readAsDataURL(file);
   }
 });
+
+/*Modelo 3D*/
+window.addEventListener('DOMContentLoaded', () => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  const container = document.getElementById('modeloBody3D');
+  if (!container) {
+    console.error('No se encontró el contenedor #modeloBody3D');
+    return;
+  }
+
+  const width = container.clientWidth;
+  const height = container.clientHeight;
+
+  renderer.setSize(width, height);
+  container.appendChild(renderer.domElement);
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(1, 1, 1).normalize();
+  scene.add(light);
+
+  const loader = new THREE.OBJLoader();
+ loader.load('../media/female_quad_obj/female_quad_obj.obj',
+  function (obj) {
+    obj.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.material = new THREE.MeshNormalMaterial(); // material temporal
+      }
+    });
+    obj.scale.set(1, 1, 1);
+    scene.add(obj);
+  },
+
+    function (xhr) {
+      console.log(`Modelo ${xhr.loaded / xhr.total * 100}% cargado`);
+    },
+    function (error) {
+      console.error('Error al cargar el modelo:', error);
+    }
+  );
+
+  camera.position.z = 5;
+
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+
+  animate();
+});
+
